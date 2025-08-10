bd695031f5a05a84e64635b8d70877572c26190b
gaagnec <gaagnec@gmail.com>
2025-08-10 22:58:58 +0200

    update bd

--- batteryRental_2/settings.py
+++ batteryRental_2/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'batteryRental_2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.yolfklentkyczxkwenis',
+        'PASSWORD': 'Y6ufkiMIJBo28V7K',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
+
+
+
+
 
 
 # Password validation
